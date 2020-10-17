f5b61bc8ff077d5d49af103ae21dd5e3bd59f283
Imam Asari <tahzan.la10@gmail.com>
2020-10-17 22:55:25 +0700

    enable local settings

--- sapujagad/settings.py
+++ sapujagad/settings.py
@@ -73,12 +73,15 @@ WSGI_APPLICATION = 'sapujagad.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+try:
+    from .local_settings import *
+except ImportError:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
