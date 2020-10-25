b4b57e9eaf40c870ff9ee5d78862ea9ba48d2de3
Imam Asari <tahzan.la10@gmail.com>
2020-10-25 19:03:45 +0700

    update

--- sapujagad/apps/settings/models.py
+++ sapujagad/apps/settings/models.py
@@ -13,14 +13,13 @@ class Settings(models.Model):
         ('our_portfolio_description', 'our_portfolio_description'),
         ('our_service_title', 'our_service_title'),
         ('our_service_description', 'our_service_description'),
+        ('our_team_title', 'our_team_title'),
+        ('our_team_description', 'our_team_description'),
         ('price_title', 'price_title'),
         ('price_description', 'price_description'),
         ('blog_title', 'blog_title'),
         ('blog_desc', 'blog_desc'),
-        ('map_title', 'map_title'),
-        ('latitude', 'latitude'),
-        ('longitude', 'longitude'),
-        ('contact_us_text', 'contact_us_text'),
+        ('contact_us_title', 'contact_us_title'),
         ('contact_us_description', 'contact_us_description'),
         ('contact_us_description_2', 'contact_us_description_2'),
         ('address', 'address'),
@@ -28,6 +27,12 @@ class Settings(models.Model):
         ('email', 'email'),
         ('website', 'website'),
         ('logo', 'logo'),
+        ('themium_title', 'themium_title'),
+        ('instagram_url', 'instagram_url'),
+        ('facebook_url', 'facebook_url'),
+        ('youtube_url', 'youtube_url'),
+        ('twitter_url', 'twitter_url'),
+
     )
     name = models.CharField(max_length=254, choices=TYPE, unique=True)
     text_value = models.TextField(blank=True, null=True)
