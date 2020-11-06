ce95961b563f254cad2b9b25442674dabfed9dcf
imamasari <imam.asari@mplus.software>
2020-11-06 22:29:53 +0700

    SEO dinamis

--- sapujagad/apps/settings/admin.py
+++ sapujagad/apps/settings/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Settings)
+
+class SettingsModelAdmin(admin.ModelAdmin):  # instead of ModelAdmin
+    ordering = ('name',)
+    list_display = ('name', 'text_value', 'img_value')
+
+admin.site.register(Settings, SettingsModelAdmin)
--- sapujagad/apps/settings/models.py
+++ sapujagad/apps/settings/models.py
@@ -4,38 +4,57 @@ from django.db import models
 
 class Settings(models.Model):
     TYPE = (
-        ('text_before_service', 'text_before_service'),
-        ('about_us_title', 'about_us_title'),
+        ('about_us_background', 'about_us_background'),
         ('about_us_description', 'about_us_description'),
         ('about_us_img', 'about_us_img'),
-        ('about_us_background', 'about_us_background'),
-        ('testimoni_title', 'testimoni_title'),
-        ('testimoni_background', 'testimoni_background'),
-        ('our_portfolio_title', 'our_portfolio_title'),
-        ('our_portfolio_description', 'our_portfolio_description'),
-        ('our_service_title', 'our_service_title'),
-        ('our_service_description', 'our_service_description'),
-        ('our_team_title', 'our_team_title'),
-        ('our_team_description', 'our_team_description'),
-        ('price_title', 'price_title'),
-        ('price_description', 'price_description'),
-        ('blog_title', 'blog_title'),
+        ('about_us_title', 'about_us_title'),
+
+        ('address', 'address'),
+
         ('blog_desc', 'blog_desc'),
+        ('blog_title', 'blog_title'),
+        
         ('contact_us_title', 'contact_us_title'),
         ('contact_us_description', 'contact_us_description'),
         ('contact_us_description_2', 'contact_us_description_2'),
-        ('address', 'address'),
-        ('phone', 'phone'),
+        
         ('email', 'email'),
-        ('website', 'website'),
-        ('logo', 'logo'),
-        ('instagram_url', 'instagram_url'),
         ('facebook_url', 'facebook_url'),
-        ('youtube_url', 'youtube_url'),
-        ('twitter_url', 'twitter_url'),
+        ('favicon', 'favicon'),
+        ('instagram_url', 'instagram_url'),
+
         ('latitude', 'latitude'),
         ('longitude', 'longitude'),
-        ('favicon', 'favicon'),
+        ('logo', 'logo'),
+
+        ('our_portfolio_title', 'our_portfolio_title'),
+        ('our_portfolio_description', 'our_portfolio_description'),
+
+        ('our_team_title', 'our_team_title'),
+        ('our_team_description', 'our_team_description'),
+
+        ('our_service_title', 'our_service_title'),
+        ('our_service_description', 'our_service_description'),
+        
+        ('testimoni_title', 'testimoni_title'),
+        ('testimoni_background', 'testimoni_background'),
+        
+        ('phone', 'phone'),
+        ('price_title', 'price_title'),
+        ('price_description', 'price_description'),
+
+        ('seo_description', 'SEO Description'),
+        ('seo_title', 'SEO Title'),
+        ('seo_keywords', 'SEO Keywords'),
+        ('seo_image', 'SEO Image'),
+        # ('seo_fb_id', 'SEO FbID'),
+        
+        ('text_before_service', 'text_before_service'),
+        ('twitter_url', 'twitter_url'),
+        
+        ('website', 'website'),
+        
+        ('youtube_url', 'youtube_url'),
     )
     name = models.CharField(max_length=254, choices=TYPE, unique=True)
     text_value = models.TextField(blank=True, null=True)
