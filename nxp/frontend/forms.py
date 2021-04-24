b5eab7fc8af1a326d756adaf1695facb253b9ef3
Imam Asari <tahzan.la10@gmail.com>
2021-04-24 22:19:36 +0700

    mantep

--- nxp/frontend/forms.py
+++ nxp/frontend/forms.py
@@ -3,9 +3,12 @@ from django import forms
 from nxp.apps.dealer.models import Dealer
 from nxp.apps.formulir.models import Formulir
 from nxp.apps.product.models import Product
+from nxp.apps.serial_number.models import SerialNumber
 
 
 class FormulirForm(forms.ModelForm):
+    dealer_code = forms.CharField(required=False)
+
     class Meta:
         model = Formulir
         fields = "__all__"
@@ -13,14 +16,15 @@ class FormulirForm(forms.ModelForm):
 
     def clean_dealer_code(self):
         code = self.cleaned_data.get("dealer_code", False)
-        if not Dealer.objects.filter(code=code).first():
+        if code and not Dealer.objects.filter(code=code).first():
             raise forms.ValidationError("Kode Dealer tidak ditemukan")
         return code
 
     def clean_serial_number(self):
         serial_number = self.cleaned_data.get("serial_number", False)
-        if not Product.objects.filter(serial_number=serial_number).first():
+        serial = SerialNumber.objects.filter(serial_number=serial_number)
+        if not serial.first():
             raise forms.ValidationError("Serial Number tidak ditemukan")
-        if Formulir.objects.filter(serial_number=serial_number).first():
+        elif serial.status == 'redeem':
             raise forms.ValidationError("Serial Number sudah digunakan")
         return serial_number
